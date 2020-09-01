b67f692b4fffece3eabc89ffe1bd0b08f3360a76
Noah Scala <nwscala@gmail.com>
2020-09-01 18:32:48 -0400

    moved fetch into ImageContainer

--- src/App.js
+++ src/App.js
@@ -4,21 +4,6 @@ import './App.css';
 import ImageContainer from './containers/ImageContainer';
 
 class App extends React.Component {
-  state = {
-    imageList: []
-  }
-  
-  componentDidMount(){
-    fetch('http://localhost:3001/images')
-      .then(resp => resp.json())
-      .then(data => this.setState({
-        imageList: [
-          ...this.state.imageList,
-          data
-        ]
-      }))
-  }
-  
   render() {
     return (
       <ImageContainer />
--- src/containers/ImageContainer.js
+++ src/containers/ImageContainer.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import Images from '../components/Images'
 
 export default class ImageContainer extends Component {
+    state = {
+        imageList: []
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3001/images')
+            .then(resp => resp.json())
+            .then(data => this.setState({
+                imageList: data
+            }))
+    }
+
     render() {
         return (
             <div>
-                
+                <Images imageList={this.state.imageList}/>
             </div>
         )
     }
